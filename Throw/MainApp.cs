e9f4c2a157036b8db9db0bb71373b608fa61169e
RND-HW3 <RND-HW3@RND-HW3>
2025-05-08 14:33:27 +0900

    초기 커밋.

---
+++ Throw/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Throw
+{
+    class MainApp
+    {
+        static void DoSomething(int arg)
+		{
+			if (arg < 10)
+			{
+				Console.WriteLine($"arg: {arg}");
+			}
+			else
+			{
+				throw new Exception($"arg가 10보다 큰 ({arg}) 입니다.");
+			}
+		}		
+		
+		static void Main(string[] args)
+        {
+			try
+			{
+				DoSomething(1);
+                DoSomething(9);
+                DoSomething(11);
+            }
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+        }
+    }
+}
